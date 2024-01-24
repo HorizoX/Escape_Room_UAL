f885b00e6290f4f804b35c0ce252d1ae6494c012
HorizoX <pt.horizon@gmail.com>
2024-01-24 23:58:51 +0000

    2nd Level Finished

---
+++ Assets/Escape Room/Scripts/Shoot_List.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot_List : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> objectsToDestroyInOrder;
+}
